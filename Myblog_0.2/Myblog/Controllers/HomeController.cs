386f74d40d5891c91252dd44480b602a0f9f6dc5
Mehmet Kemal Yıldırım <mehmet.kemal111@gmail.com>
2020-10-07 18:24:38 +0300

    biz

--- Myblog_0.2/Myblog/Controllers/HomeController.cs
+++ Myblog_0.2/Myblog/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using Myblog.Models;
 
 namespace Myblog.Controllers
 {
@@ -10,6 +13,49 @@ namespace Myblog.Controllers
     {
         public ActionResult Index()
         {
+          
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(modaller model)
+        {
+            bool sonuc = false;
+
+            WebMail.SmtpServer = ConfigurationManager.AppSettings["emailHost"];
+            WebMail.SmtpPort = int.Parse(ConfigurationManager.AppSettings["emailPort"]);
+            WebMail.UserName = ConfigurationManager.AppSettings["emailAccount"];
+            WebMail.Password = ConfigurationManager.AppSettings["emailPassword"];
+            WebMail.EnableSsl = true;
+
+            // ek dosya alabilmek için
+            //string file = Server.MapPath("~/img/map-image.png");
+
+            try
+            {
+                WebMail.Send(
+                    to: "digizevahir@gmail.com", subject: "Bize Ulaşan "+model.name_surname,
+                    body: "Mesajı  " + model.massage+ "//==> Telefon Numarası :" + model.number,
+                    replyTo: model.email, isBodyHtml: true
+                   // ,filesToAttach: new[] { file }
+                    );
+
+                sonuc = true;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Hata = ex.Message;
+            }
+
+            ViewBag.Sonuc = sonuc;
+            if (sonuc == true)
+            {
+                TempData["Basarili"] = "Teşekürler en kısa zamanda tafarınıza dönüş yapılacaktır.";
+
+                return RedirectToAction("Index");
+            }
+
             return View();
         }
 
---
+++ Myblog_0.2/Myblog/Models/modaller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Myblog.Models
+{
+    public class modaller
+    {
+        [Required(ErrorMessage = "AdıSoyadı gereklidir!!!")] 
+        public string name_surname { get; set; }
+
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [EmailAddress(ErrorMessage = "Email Geçersiz")]
+        public string email { get; set; }
+     
+
+        public DateTime date { get; set; }
+
+        [Required(ErrorMessage = "Boş Olmaz!!!")]
+        [MaxLength(500, ErrorMessage = "500 karateri geçmeyin")]
+
+        public string massage { get; set; }
+        
+
+        public string number { get; set; }
+
+
+    }
+    }
